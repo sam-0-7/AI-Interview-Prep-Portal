1759f3b5288536366fa46450881304f56cf88d81
sam-0-7 <sam390583@gmail.com>
2026-07-16 22:54:43 +0530

    debug: add temporary /debug-db diagnostics endpoint to HomeController

--- AIInterviewPortal/Controllers/HomeController.cs
+++ AIInterviewPortal/Controllers/HomeController.cs
@@ -28,5 +28,39 @@ namespace AIInterviewPortal.Controllers
         [AllowAnonymous]
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error() => View();
+
+        [AllowAnonymous]
+        [Route("debug-db")]
+        public async Task<IActionResult> DebugDb()
+        {
+            try
+            {
+                var techCount = await _db.Technologies.CountAsync();
+                var userCount = await _db.Users.CountAsync();
+                var conn = _db.Database.GetConnectionString();
+                var maskedConn = conn;
+                if (conn != null && conn.Contains("Password="))
+                {
+                    var idx = conn.IndexOf("Password=");
+                    var endIdx = conn.IndexOf(";", idx);
+                    if (endIdx == -1) endIdx = conn.Length;
+                    maskedConn = conn.Substring(0, idx) + "Password=***" + conn.Substring(endIdx);
+                }
+                return Content($"Database connection SUCCESS! Technologies count: {techCount}, Users count: {userCount}. Conn: {maskedConn}");
+            }
+            catch (Exception ex)
+            {
+                var conn = _db.Database.GetConnectionString();
+                var maskedConn = conn;
+                if (conn != null && conn.Contains("Password="))
+                {
+                    var idx = conn.IndexOf("Password=");
+                    var endIdx = conn.IndexOf(";", idx);
+                    if (endIdx == -1) endIdx = conn.Length;
+                    maskedConn = conn.Substring(0, idx) + "Password=***" + conn.Substring(endIdx);
+                }
+                return Content($"Database connection FAILED: {ex.Message}\n\nStack Trace:\n{ex.StackTrace}\n\nInner Exception:\n{ex.InnerException?.Message}\n\nConn: {maskedConn}");
+            }
+        }
     }
 }
